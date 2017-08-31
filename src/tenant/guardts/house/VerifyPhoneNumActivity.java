5dc35f278d163071297b4cbc52bc8eb4993316e8
applewang2012 <applewang2012@sina.com>
2017-08-31 11:33:35 +0800

    about  withdraw  layout by zhou

---
+++ src/tenant/guardts/house/VerifyPhoneNumActivity.java
@@ -0,0 +1,12 @@
+package tenant.guardts.house;
+
+import android.os.Bundle;
+
+public class VerifyPhoneNumActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_verify_phone_num);
+	}
+}
